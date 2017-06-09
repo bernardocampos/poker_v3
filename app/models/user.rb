1119405bdf565526bfff17ed1c11ca066cb5ebef
bernardocampos <bernardocampos@users.noreply.github.com>
2017-06-09 10:37:01 -0500

    updated timeout rule, created kanban

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
+  devise :timeoutable, :timeout_in => 2880.minutes
   has_many :players, :dependent => :destroy
   has_many :tables, :through => :players
 
